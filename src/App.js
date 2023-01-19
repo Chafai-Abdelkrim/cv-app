9487b7aa2d85c31fe087276dac7dffbdb57db5eb
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-19 18:08:57 +0100

    cleaned up the indexjs and appjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { createRef, useState } from 'react';
+import { useTransition } from 'react-transition-state';
+import { useScreenshot } from 'use-react-screenshot';
+import { saveAs } from 'file-saver';
+import uniqid from 'uniqid';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
