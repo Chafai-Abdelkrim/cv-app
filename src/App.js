2ceb6be0a4e52e507e4a20ecc37a3af1fa9436ac
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-20 10:25:43 +0100

    added useState to component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,33 @@ import { useTransition } from 'react-transition-state';
 import { useScreenshot } from 'use-react-screenshot';
 import { saveAs } from 'file-saver';
 import uniqid from 'uniqid';
-
+import Editor from './components/Editor';
+import Preview from './components/Preview';
+import './styles/App.css';
 
 function App() {
+  const createItems = (count) => {
+    return Array(count)
+      .fill()
+      .map(() => ({ key: uniqid() }));
+  };
+
+  const [data, setData] = useState({
+    info: {},
+    contact: {},
+    education: createItems(1),
+    work: createItems(1),
+    skills: createItems(3),
+  });
+
+  const screenshotRef = createRef(null);
+  const transformRef = createRef(null);
+  const [, takeScreenshot] = useScreenshot({ type: 'image/png', quality: 1.0 });
+  const [previewState, togglePreview] = useTransition({ timeout: 300 });
+  const getImage = () => {
+    transformRef.current?.centerView(1, 0);
+    takeScreenshot(screenshotRef.current).then(saveAs);
+  };
   
 }
 
