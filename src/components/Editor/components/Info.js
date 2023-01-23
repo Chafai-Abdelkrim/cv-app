5824f6df00f928fdecc8282030f620ed132711ed
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-23 10:33:45 +0100

    created Info component

---
+++ src/components/Editor/components/Info.js
@@ -0,0 +1,41 @@
+import Form from './Form';
+import uniqid from 'uniqid';
+
+const inputFields = [
+  {
+    label: 'First Name',
+    placeholder: 'Your First Name',
+  },
+  {
+    label: 'Last Name',
+    placeholder: 'Your Last Name',
+  },
+  {
+    label: 'Professional Title',
+    placeholder: 'Your Profession',
+  },
+  {
+    label: 'Profile',
+    placeholder: 'I am a Web Developer with special love to React...',
+    type: 'textarea',
+  },
+];
+inputFields.forEach((field) => (field.key = uniqid()));
+
+function Info({ data, setData }) {
+  const { info } = data;
+  const handleInput = (key, value) => {
+    setData({ ...data, info: { ...info, [key]: value } });
+  };
+
+  return (
+    <Form
+      className="info"
+      inputFields={inputFields}
+      data={info}
+      handleInput={handleInput}
+    />
+  );
+}
+
+export default Info;
