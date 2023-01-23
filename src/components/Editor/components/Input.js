8cf7b1f83144116571581219668a0360d46d0756
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-23 09:34:37 +0100

    create Input component

---
+++ src/components/Editor/components/Input.js
@@ -0,0 +1,40 @@
+import camelCase from 'camelcase';
+
+function Input(props) {
+  const {
+    label,
+    placeholder,
+    data,
+    handleInput,
+    value = '',
+    id = camelCase(label || ''),
+    type = 'text',
+  } = props;
+
+  return (
+    <div className="Input">
+      {label && <label htmlFor={id}>{label}</label>}
+      {type === 'textarea' ? (
+        <textarea
+          id={id}
+          name={id}
+          placeholder={placeholder}
+          value={data?.[id] || value}
+          onInput={(e) => handleInput(id, e.target.value)}
+        />
+      ) : (
+        <input
+          id={id}
+          name={id}
+          type={type}
+          placeholder={placeholder}
+          value={data?.[id] || value}
+          onInput={(e) => handleInput(id, e.target.value)}
+        />
+      )}
+    </div>
+  );
+}
+
+
+export default Input;
