5757d9eb1965fb515b30732a097fe6c694acd5b2
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-23 09:44:01 +0100

    created Form component

---
+++ src/components/Editor/components/Form.js
@@ -0,0 +1,22 @@
+import Input from './Input';
+
+function Form(props) {
+  const { className, inputFields, data, handleInput } = props;
+
+  return (
+    <form className={className}>
+      {inputFields.map(({ label, placeholder, type, key }) => {
+          <Input 
+            key={key}
+            label={label}
+            placeholder={placeholder}
+            type={type}
+            data={data}
+            handleInput={handleInput}
+          />
+      })}
+    </form>
+  );
+}
+
+export default Form;
