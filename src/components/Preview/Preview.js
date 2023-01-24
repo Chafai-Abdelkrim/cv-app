ff94e5396d779ca367d2b6e1b63b90c85b8a8005
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-24 16:11:58 +0100

    added the clamp function

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,5 +1,15 @@
-function Preview(props) {
+import { useEffect } from 'react';
+import { useWindowSize } from '@react-hook/window-size';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import Zoom from './components/Zoom';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Experience from './components/Experience';
+import clamp from '../../utils/clamp';
+import '../../styles/Preview.css';
 
+function Preview(props) {
+    
 }
 
 export default Preview;
---
+++ src/utils/clamp.js
@@ -0,0 +1,5 @@
+function clamp(min, value, min) {
+  return Math.max(min, Math.min(value, max));
+}
+
+export default clamp;
