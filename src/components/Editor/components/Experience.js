ddc8944cf844907742e01b2e56107d0a90391a4d
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-24 08:44:17 +0100

    created Experience component

---
+++ src/components/Editor/components/Experience.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import uniqid from 'uniqid';
+import Form from './Form';
+import Button from '../../Button';
+import Navigator from './Navigator';
+import { RiAddLine, RiDeleteBin6Line } from 'react-icons/ri';
+
+function Experience(props) {
+  const { className, items, setItems, inputFields } = props;
+  const [index, setIndex] = useState(0);
+
+  const createItem = () => {
+    const newItems = [...items];
+    newItems.push({ key: uniqid() });
+    setIndex(index + 1);
+    setItems(newItems);
+  };
+
+  const deleteItem = () => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    if (index === newItems.length) setIndex(index - 1);
+    setItems(newItems);
+  };
+
+  const handleInput = (key, value) => {
+    const newItems = [...items];
+    newItems[index][key] = value;
+    setItems(newItems);
+  };
+
+  const newButton = (
+    <Button icon={<RiAddLine />} lable="New" handleClick={createItem} alt />
+  );
+
+  return (
+    <div className="Container">
+      {items.length > 1 && (
+        <Button
+          className="Delete"
+          icon={<RiDeleteBin6Line />}
+          title="Delete"
+          handleClick={deleteItem}
+          alt
+        />
+      )}
+      <Form
+        className={className}
+        inputFields={inputFields}
+        data={items[index]}
+        handleInput={handleInput}
+      />
+      <Navigator
+        index={index}
+        setIndex={setIndex}
+        items={items}
+        lastButton={items.length < 3 && newButton}
+      />
+    </div>
+  );
+}
+
+export default Experience;
