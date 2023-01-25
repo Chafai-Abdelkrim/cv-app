c2a48e050e8ebf63da84c92dc17eae4e5d555cf7
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-25 09:21:34 +0100

    created Contact component

--- src/components/Preview/components/Contact.js
+++ src/components/Preview/components/Contact.js
@@ -5,3 +5,19 @@ import {
   RiPhoneLine,
 } from 'react-icons/ri';
 import Detail from './Detail';
+
+function Contact({ contact }) {
+  const { website, email, location, phone } = contact;
+
+  return (
+    <section className="Contact">
+      <h3>CONTACT</h3>
+      <Detail icon={<RiGlobalLine />} label={website} capitalized={false} />
+      <Detail icon={<RiAtLine />} label={email} capitalized={false} />
+      <Detail icon={<RiMapPin2Line />} label={location} />
+      <Detail icon={<RiPhoneLine />} label={phone} capitalized={false} />
+    </section>
+  );
+}
+
+export default Contact;
