bd1f4630a9d089d25c1a4f8618bccee086c3fe7e
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-25 16:03:57 +0100

    created Sidebar component

---
+++ src/components/Preview/components/Sidebar.js
@@ -0,0 +1,15 @@
+import Contact from './Contact';
+import Experience from './Experience';
+import Skills from './Skill';
+
+function Sidebar({ contact, education, skills }) {
+  return (
+    <div className="Sidebar">
+      <Contact contact={contact} />
+      <Experience name="Education" education={education} />
+      <Skills skills={skills} />
+    </div>
+  );
+}
+
+export default Sidebar;
